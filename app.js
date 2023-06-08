c412c5236f73ba7aa9e201782154c76b932e2229
bpsrm <bankppt.srm@gmail.com>
2023-06-09 00:55:18 +0700

    commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('layout', { body: 'index' });
+});
+
+app.get('/simulacrum', (req, res) => {
+    res.render('layout', { body: 'simulacrum' });
+});
+
+app.get('/event', (req, res) => {
+    res.render('layout', { body: 'event' });
+});
+
+app.get('/loading', (req, res) => {
+    res.render('loading');
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`);
+});
